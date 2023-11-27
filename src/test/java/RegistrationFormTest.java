8c969e356ccb5a528b8bdf94227c215999414062
elenapltnkv <elenapltnkv@gmail.com>
2023-11-27 11:57:42 +0300

    try test

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -1,17 +1,19 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.checked;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-public class StudentRegistrationForm {
+public class RegistrationFormTest {
     @BeforeAll
     static void beforeAll(){
-        Configuration.baseUrl= "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        baseUrl= "https://demoqa.com";
     }
     @Test
     public void registrationForm(){
@@ -19,8 +21,10 @@ public class StudentRegistrationForm {
         $("#firstName").setValue("Elena");
         $("#lastName").setValue("Samelena");
         $("#userEmail").setValue("samtest@test.ru");
-        //$(".custom-control-input").shouldHave(text("Female"));
-        //$(".btn btn-primary").click();
+        //$(By.cssSelector("input[id='gender-radio-2']")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#submit").click();
+
 
     }
 
